5c8d05232838a7921af0409aad53df933cefd3f3
Jay Lakdawala <jdlakdawala19@gmail.com>
2024-09-05 20:56:25 +0530

    Test for a single character in a string

--- tests/string-calculator.test.js
+++ tests/string-calculator.test.js
@@ -3,3 +3,7 @@ const stringCalculator = require('../src/string-calculator');
 test('should return 0 for an empty string', () => {
   expect(stringCalculator.add('')).toBe(0);
 });
+
+test('should return the number itself for a single number', () => {
+  expect(stringCalculator.add('1')).toBe(1);
+});
